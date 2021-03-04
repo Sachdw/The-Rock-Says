b7917ebc2ff0b89f42f3c647081a03089423e761
Sacha <40357915+Sachdw@users.noreply.github.com>
2021-03-04 21:23:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,18 @@ embedding_dim = 256
 # Number of RNN units
 rnn_units = 1024
 
+#Define the model architecture and load pretrained weights
 model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
 model.load_weights('theRock_weights70.h5')
 model.build(TensorShape([1, None]))
 
 
+#Display the main page of the app
 @app.route('/')
 def home():
     return render_template('index.html')
 
+#When users click the 'generate' button, use the model to generate text
 @app.route('/', methods=['POST', 'GET'])
 def get_data():
     start = request.form['first_word']
